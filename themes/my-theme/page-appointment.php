7d821676a7e1473a4ef06c952fc77d620c1cdf24
sokcheadalinsorn <sokcheadalinsorn@gmail.com>
2025-12-25 16:33:00 +0700

    do appointment page

---
+++ themes/my-theme/page-appointment.php
@@ -0,0 +1,57 @@
+<link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
+
+
+<div class="header_appointment">
+    <div class="appontment">
+        <a href="#"><p>Appointment</p></a>
+    </div>
+    <div class="logo_left">
+        <img src="http://ohana-clinic.test/wp-content/uploads/2025/12/ChatGPT_Image_Dec_25__2025__10_17_21_AM-removebg-preview.png" alt="logo">
+    </div>
+</div>  
+<div class="line">
+
+</div>
+
+<!-- form_contact_section -->
+<div class="container_form_contact">
+    <div class="form_contact">
+        <p class="description">
+        Request an Appointment <br>
+        PLease complete the form below to request an appointment. Our team will review your information and reach out to comfirm the date and time. If you need immediate <br>
+        assistance or have aan urgent matter, please call us directly using the contact information provided.
+        </p>
+
+        <div class="contact_form">
+            <form action="action_page.php">
+                <label for="fname">Doctors</label><br>
+                <input type="text" id="fname" name="fname" value="Choose the doctor from the list"><br>
+                <label for="fname">First name</label><br>
+                <input type="text" id="fname" name="fname" value="Enter your first name"><br>
+                <label for="lname">Last name</label><br>
+                <input type="text" id="lname" name="lname" value="Enter your last name"><br>
+                <label for="lname">Sex</label><br>
+                <input type="text" id="lname" name="lname" value="Select your sex">
+                <label for="lname">Date of birth</label><br>
+                <input type="text" id="lname" name="lname" value="DD/MM/YY or use picker">
+                <label for="lname">Phone Number</label><br>
+                <input type="text" id="lname" name="lname" value="+855(KH)">
+                <label for="lname">Email Address</label><br>
+                <input type="text" id="lname" name="lname" value="Enter you email">
+                <label for="reason">Reason for appointment</label>
+                <textarea id="reason" name="reason" placeholder="Write something.." style="height:200px"></textarea>
+                
+                
+            </form> 
+        </div>
+
+       
+
+
+
+
+
+    </div>
+
+    
+</div>
