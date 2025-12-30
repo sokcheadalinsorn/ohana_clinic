244350c21bd9d1c8ef6096e25aa928aee2112b2b
vannararin23-sketch <vannararin23@gmail.com>
2025-12-30 08:21:29 +0700

    do home page not yet finish

---
+++ themes/my-theme/header.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
+<header>
+    <div class="navbar">
+        <div class="logo">
+             <img src="http://ohana-clinic.test/wp-content/uploads/2025/12/logo.jpg" alt="">
+        </div>
+        <div class="menu">
+               <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">ABOUT INTERCARE</a></li>
+                <li><a href="">CONTACT US</a></li>
+                <li><a href="">EMERGENCY</a></li>
+                <li><a href="">FIND DOCTORS</a></li>
+                <li><a href="">APPOINTMENTS</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
+<?php get_footer();?>
+ 
